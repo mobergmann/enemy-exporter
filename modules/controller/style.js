a855050a0aac85969cb9879cc3a7d262b303b4f6
mobergmann <mobergmann@pm.me>
2021-08-27 14:14:04 +0200

    now a custom stylesheet can be loaded

--- modules/controller/style.js
+++ modules/controller/style.js
@@ -1,14 +1,22 @@
-function changeStyle() {
-    console.log("Hello World");
-    let callback = (style_url) => {
-        let export_stylesheet = document.getElementById("setting-stylesheet");
-        export_stylesheet.href = style_url;
-    };
+function set_style_url(style_url) {
+    let export_stylesheet = document.getElementById("setting-stylesheet");
+    export_stylesheet.href = style_url;
+}
 
+function set_style(css_text) {
+    let style_container = document.getElementById("setting-container");
+    style_container.innerHTML = css_text;
+}
+
+function changeStyle() {
+    // reset previously set styles
+    set_style_url(null);
+    set_style(null);
 
     let choice = document.getElementById("input-style").value.toLowerCase();
     switch (choice) {
         case "custom":
+            // get file from input
             let file = document.getElementById("input-style-file").files[0];
             if (!file) {
                 alert("You need to upload a JSON-File, which defines all Attributes.\n\n" +
@@ -17,14 +25,20 @@ function changeStyle() {
                 return;
             }
 
-            // todo
-            alert("TODO");
+            // set file input as style
+            let reader = new FileReader();
+            reader.onload = function(e) {
+                let contents = e.target.result;
+                set_style(contents);
+            };
+            // read file text
+            reader.readAsText(file);
 
             break;
 
         default:
             let url = "themes/" + choice + ".css";
-            callback(url);
+            set_style_url(url);
             break;
     }
 }
